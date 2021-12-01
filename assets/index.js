ea69839939672afba69d78b7d0c6307cdfc48164
jhon garcia <jagg499@gmail.com>
2021-12-01 17:08:42 -0500

    agrego un plugin de autoplay

--- assets/index.js
+++ assets/index.js
@@ -1,22 +1,15 @@
-        //selecciono los elementos button y video y los almaceno en las variables
-        const button = document.querySelector("button");
-        const video  = document.querySelector("video");
+import MediaPlayer  from "./mediaPlayer.js";
+import Autoplay from "../Plugins/Autoplay.js";
 
-        //creo un objeto le paso como parametro un objeto de configuracion
-        function MediaPlayer(config){
-            //almaceno el elemento de la configuracion en la variable media
-            this.media = config.element;
-        };
+//selecciono los elementos button y video y los almaceno en las variables
+const playButton = document.getElementById("play");
+const unmuteButton = document.getElementById("unmute");
+const video = document.querySelector("video");
 
-        //creo un metodo del objeto mediaplayer que reproduce la media
-        MediaPlayer.prototype.play = function(){
-            (this.media.paused)?
-                this.media.play():
-                this.media.pause();
-        }
+//creo una instancia y le paso en la configuracion el video
+const player = new MediaPlayer({ element : video , pluggins : [new Autoplay()] });
 
-        //creo una instancia y le paso en la configuracion el video
-        const player = new MediaPlayer({ element : video });
+//al hacer click invoco el metodo play de el objeto player
+playButton.onclick = () => player.playMedia();
+unmuteButton.onclick = () => player.unmute();
 
-        //al hacer click invoco el metodo play de el objeto player
-        button.onclick = () => player.play();
